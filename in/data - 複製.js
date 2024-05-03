0c6fec4ce4b09d6fa0eb2ae95e26f6e8a38173c8
fdse456 <160987138+fdse456@users.noreply.github.com>
2024-05-03 11:28:55 +0800

    dg

---
+++ in/data - 複製.js
@@ -0,0 +1,259 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "瀑布",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.906241130211317,
+        "pitch": 0.18031725654447328,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4051422851804887,
+          "pitch": 0.156367441770918,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7813378806504794,
+          "pitch": -0.1639041265894292,
+          "title": "中華路二段和中誠街路口",
+          "text": "向全國加油站的方向進入中誠街,有一家好吃的鍋燒意麵叫「阿雞師」"
+        },
+        {
+          "yaw": 2.626416051508963,
+          "pitch": 0.19432984199340453,
+          "title": "圍牆",
+          "text": "這裡常有鳥類戲水"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "木橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 3.05907597216816,
+        "pitch": 0.13253664174789392,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16921547945057824,
+          "pitch": 0.3583881702747256,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 3.110123355835645,
+          "pitch": 0.2897089242070461,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.745411080237229,
+          "pitch": 0.3667061424086526,
+          "title": "鳶尾",
+          "text": "開花植物，屬於鳶尾科，主要分布在北非、西班牙、葡萄牙、高加索等地區"
+        },
+        {
+          "yaw": -1.0968155504128294,
+          "pitch": 0.25874156524865377,
+          "title": "圍牆",
+          "text": "這裡常有烏龜爬出水曬太陽"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "未建的浮橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 3.1283164941590336,
+        "pitch": 0.039087472391408085,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02230688555203386,
+          "pitch": 0.29067068676581265,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7870693112145295,
+          "pitch": 0.13611322882059973,
+          "title": "未建的浮橋",
+          "text": "這張照片的拍攝時間這裡還未建立浮橋，不過現在這裡有一座浮橋，可以一路走到宏匯廣場"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "荒涼路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -3.0813158561920595,
+        "pitch": 0.2847569701483792,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0813158561920595,
+          "pitch": 0.2847569701483792,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.0050703384157611,
+          "pitch": 0.1978841762402901,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2352742593523445,
+          "pitch": 0.06875764868352796,
+          "title": "圍牆",
+          "text": "遇到節慶時可能會有燈光表演"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "複雜交叉口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6179754617717865,
+          "pitch": 0.11787033129759195,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.7968919636939154,
+          "pitch": 0.1530753324484042,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ out/data - 複製.js
@@ -0,0 +1,418 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "大大排",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1325112381291884,
+          "pitch": 0.1763542755817138,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -0.013117181109038256,
+          "pitch": 0.23472917689425898,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1423460735682465,
+          "pitch": 0.33224188232890306,
+          "title": "中港大排",
+          "text": "中港大排和塭仔圳交會，變得更寬了"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "光雕",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.951147315167275,
+          "pitch": 0.204693450192444,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.22242696957470898,
+          "pitch": 0.06817362625303325,
+          "title": "未建的土地公廟",
+          "text": "這裡現在有一座香火旺盛的土地公廟"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "抽水站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.3233078480302112,
+        "pitch": 0.15314142876913195,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6881806018478436,
+          "pitch": 0.22839694200073346,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.6238262676108253,
+          "pitch": 0.20779015306273863,
+          "rotation": 0,
+          "target": "3-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2",
+      "name": "抽水站2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6075785745480076,
+        "pitch": 0.08197519550674137,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4987017092179791,
+          "pitch": 0.31196038447132324,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8485769972127901,
+          "pitch": -0.5945251765005803,
+          "title": "Title",
+          "text": "改善惡臭並提高整體防洪、疏濬功能，預防極端強降雨不及啟動機組運作，在汛期已至的當下將能減緩暴雨帶來的致災風險及災害防治，營造親水景觀休閒帶，並建構民眾親水、遊憩的友善環境。"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "彩虹廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -2.992068990167999,
+        "pitch": -0.07210629728555773,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14185590173418916,
+          "pitch": 0.36951767643112277,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": -3.0465271186298537,
+          "pitch": 0.2662807640561553,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "彩繪橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -2.3063265156914543,
+        "pitch": 0.17408854229184456,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4194659288942493,
+          "pitch": 0.13521940824367817,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.7123711859705804,
+          "pitch": 0.4535893827259514,
+          "rotation": 0.7853981633974483,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "港邊公園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.6931759959438004,
+        "pitch": 0.27536018824721964,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1269762512009951,
+          "pitch": 0.2451624004209627,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.1310800755566817,
+          "pitch": 0.3561319770649334,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "資料館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.3602296324655825,
+        "pitch": -0.286222542144305,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3916452507262971,
+          "pitch": 0.20819580285004236,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -3.051139335267017,
+          "pitch": 0.30877621168117564,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6980948515603167,
+          "pitch": -0.17538216899125914,
+          "title": "已消失的資料館",
+          "text": "這裡現在是宏匯廣場"
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "鴿橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3091378670968687,
+          "pitch": 0.1890700132128309,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.7702781087290855,
+          "pitch": 0.30277319546788384,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.26696725385456155,
+          "pitch": -0.6841950271917572,
+          "title": "昌中橋",
+          "text": "這裡常有大群鴿子出沒"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
